7830e25cab790e214b9acc4c4daa4b49302345f8
RobinPMT <phanminhtuan023654@gmail.com>
2020-10-28 17:29:36 +0700

    config mail

--- config/mail.php
+++ config/mail.php
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME', 'phanminhtuan023654@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD','zzbziiiqjksddheo'),
 
     /*
     |--------------------------------------------------------------------------
