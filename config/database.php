73731f74fc92f4466b821c5bc1e869b0af37d9d0
RobinPMT <phanminhtuan023654@gmail.com>
2020-11-12 14:13:21 +0700

    config

--- config/database.php
+++ config/database.php
@@ -40,7 +40,7 @@ return [
         ],
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'https://www.db4free.net/'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravelgearshop'),
             'username' => env('DB_USERNAME', 'robinpmt'),
